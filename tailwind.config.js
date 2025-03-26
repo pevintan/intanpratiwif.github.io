5f6a7aab5b3cb83582b6371e1ac5c0df28348e8a
Muhammad Azra Sabila <azrasabila@gmail.com>
2025-03-26 14:03:53 +0700

    feat: add index

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          jakarta: ['Plus Jakarta Sans', 'sans-serif'],
+        },
+        colors: {
+          secret: {
+            background: '#222',
+            text: '#fff',
+            link: '#ff6347',
+          },
+        },
+      },
+    },
+    plugins: [],
+  };
+  
